462b5465f33f19340cd9372ed7ae728381bc5997
Vladislav Antonenko <36720380+VladislavAntonenko@users.noreply.github.com>
2023-12-13 10:19:47 +0300

    It's small blog on PHP language

---
+++ PHP_blog-master/blog/index.php
@@ -0,0 +1,31 @@
+<?php
+    require_once ("database.php");
+    require_once ("models/articles.php");
+
+    $link = db_connect();
+    $articles = articles_all($link);
+    require_once ("views/articles.php");
+
+
+
+
+
+
+
+
+//    //echo "Hello, world!<br>";
+//   //echo time();
+//    function add($param1, $param2)  {
+//        return $param1 + $param2;
+//    }
+//
+//
+//    $a = $_GET['a'];
+//    $b = $_GET['b'];
+//
+//    echo  add($a, $b );
+//    echo "<br>";
+//    for ($i=0;$i<10;$i++)   {
+//        echo $i."<br>";
+//    }
+?>
